eed4eefa7541365e265def71ca484c200e52c130
Meier <meier.link24@gmail.com>
2025-04-05 21:53:27 +0000

    nav bar

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
-const right = document.getElementById("right-arrow");
-const left = document.getElementById("left-arrow");
-const content = document.getElementById("content");
-
-right.addEventListener("click", () => {
-    
-content.innerHTML = '<h1 class="header_1"> STEX </h1> <p class="para1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente laborum minus maiores quaerat, eius natus assumenda quisquam sit quas ratione voluptatum facilis omnis quasi labore est voluptatem fugiat, porro reiciendis!Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi provident veritatis, culpa dignissimos, laboriosam laudantium necessitatibus assumenda vitae nihil ducimus itaque suscipit, obcaecati pariatur consectetur a et libero accusamus sequi.</p> '
-}); 
-
-left.addEventListener("click", () => {
-content.innerHTML = '<h1 class= "header_1"> about me </h1>';
-});
+const HOME = document.getElementById("Home");
+const HOME = document.getElementById("About me");
+const HOME = document.getElementById("Portfolio");
+const HOME = document.getElementById("Content");
